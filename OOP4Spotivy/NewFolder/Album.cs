7bab7145b2033ab610d1e2812c774cd167966ffe
Pim Melchers <1210025@student.roc-nijmegen.nl>
2025-06-04 08:48:51 +0200

    Album and playlist

--- OOP4Spotivy/NewFolder/Album.cs
+++ OOP4Spotivy/NewFolder/Album.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 public class Album : SongCollection
 {
     public List<Artist> Artists { get; set; }
+    public List<Song> Songs { get; }
 
-    public Album(List<Artist> artists, string title, List<Song> songs) : base(title) { }
-    public List<Artist> ShowArtists() { return null; }
-    public override string ToString() => base.ToString();
+    public Album(List<Artist> artists, string title, List<Song> songs) : base(title)
+    {
+        Artists = artists;
+        Songs = songs;
+    }
+
+    public List<Artist> ShowArtists() => Artists;
+    public List<Song> ShowSongs() => Songs;
+
+    public override string ToString() => $"{Title} ({Artists.Count} artists, {Songs.Count} songs)";
 }
